4136c57853cd1e8c9ce62a2f1595b44760fe7aff
tsw6 <keechuah@gmail.com>
2018-12-03 20:48:41 +0800

    commit

---
+++ day6/index.js
@@ -0,0 +1,39 @@
+//invisible
+
+
+/*
+
+This is invisible
+
+*/
+
+
+
+function calculate(myLocation, myLocation2, myLocation3) {
+    var myName ="This is Stackup, at " + myLocation;
+    console.log(myName);
+}
+
+calculate("One North", "JTC Launchpad");
+calculate("abtest", "test");
+
+function getMyFullName(){
+
+    var firstName = "Stackup";
+    var lastName = "One North";
+    var fullName = firstName + " " + lastName;
+    return fullName;
+}
+
+var myFullName = getMyFullName();
+console.log(myFullName);
+
+function calculate(firstName, lastName){
+    var greetingName = "Hello," + firstName + "" + lastName
+    console.log(greetingName);
+}
+
+calculate ("michael", "jordan")
+calculate ("mauro","kardi")
+calculate ("lionel" , "messi")
+
